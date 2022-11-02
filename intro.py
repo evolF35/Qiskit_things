2e120125787c4e9f3749e29b23923e795db112a7
evolF35 <maccyair@gmail.com>
2022-11-02 03:28:10 -0700

    clunky working

---
+++ intro.py
@@ -0,0 +1,37 @@
+import numpy as np
+from qiskit import QuantumCircuit, transpile
+from qiskit.providers.aer import QasmSimulator
+from qiskit.visualization import plot_histogram
+
+simulator = QasmSimulator()
+
+
+print("hello World")
+
+circuit = QuantumCircuit(2, 2)
+circuit.h(0)
+
+circuit.cx(0, 1)
+
+# Map the quantum measurement to the classical bits
+circuit.measure([0,1], [0,1])
+
+# compile the circuit down to low-level QASM instructions
+# supported by the backend (not needed for simple circuits)
+compiled_circuit = transpile(circuit, simulator)
+
+# Execute the circuit on the qasm simulator
+job = simulator.run(compiled_circuit, shots=1000)
+
+# Grab results from the job
+result = job.result()
+
+# Returns counts
+counts = result.get_counts(compiled_circuit)
+print("\nTotal count for 00 and 11 are:",counts)
+
+# Draw the circuit
+circuit.draw()
+
+plot_histogram(counts)
+
